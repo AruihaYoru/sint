aee7f392bfa05d4f0603c3ccdd94f653c27ff7b2
AruihaYoru <AruihaYoru@gmail.com>
2026-09-20 17:06:55 +0900

    replace

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sint-org.com',
+  redirects: {
+    '/chronicle': 'https://sint-org.academia.edu/KoheiOkawa',
+  },
 });
